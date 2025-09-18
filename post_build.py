06b8df05c211d6a3f4d1f25032df81f7d9217130
kakuturinithish <naninithish12345@gmail.com>
2025-09-18 17:28:28 +0530

    Update post_build.py

--- post_build.py
+++ post_build.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 print("Downloading translation model...")
-pipeline('translation', model='Helsinki-NLP/opus-mt-pt-en')"
-pipeline('translation', model='Helsinki-NLP/opus-mt-es-en')"
-pipeline('translation', model='Helsinki-NLP/opus-mt-fr-en')"
-pipeline('translation', model='Helsinki-NLP/opus-mt-de-en')"
+pipeline('translation', model='Helsinki-NLP/opus-mt-pt-en')
+pipeline('translation', model='Helsinki-NLP/opus-mt-es-en')
+pipeline('translation', model='Helsinki-NLP/opus-mt-fr-en')
+pipeline('translation', model='Helsinki-NLP/opus-mt-de-en')

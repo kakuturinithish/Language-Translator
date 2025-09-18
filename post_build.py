397bea263b7a6bbcb12acc2fbd8815da47a6b848
kakuturinithish <naninithish12345@gmail.com>
2025-09-18 18:38:52 +0530

    Delete post_build.py

--- post_build.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-print("Downloading translation model...")
-pipeline('translation', model='Helsinki-NLP/opus-mt-es-en')
-pipeline('translation', model='Helsinki-NLP/opus-mt-fr-en')
-pipeline('translation', model='Helsinki-NLP/opus-mt-de-en')
